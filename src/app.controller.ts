6c80b8f7bfcd9d21d45d9a7db7112170cc0dabec
Shafi Rayhan <rayhanshafi7@gmail.com>
2021-10-02 16:29:27 +0600

    show all avilable apis on indexRoute

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,28 @@
 import { Controller, Get } from '@nestjs/common';
-import { AppService } from './app.service';
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+  constructor() {}
 
   @Get()
-  getHello(): string {
-    return this.appService.getHello();
+  indexRoute() {
+    return `
+      <h1>NestJS Shop API</h1>
+      <h2>All the Routes</h2>
+      <ul>
+        <li>
+          Get all Products @ <a href="/products">/products</a>
+        </li>
+        <li>
+          Get all Products of Category @ <a href="/proucts/:id">/proucts/:id</a>
+        </li>
+        <li>
+          Get Products in Price Range @ <a href="/products/price/min-max">/products/price/{min}-{max}</a>
+        </li>
+        <li>
+          Get Products in Rating Range @ <a href="/products/rating/min-max">/products/rating/{min}-{max}</a>
+        </li>
+      </ul>
+    `;
   }
 }
